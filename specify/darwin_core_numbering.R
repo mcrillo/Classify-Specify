7f7ae144480a48a708198a84bdac6633aa3341b8
Marina Costa Rillo <marina.rillo@evobio.eu>
2018-05-17 13:41:59 +0200

    specimens by hand

--- specify/darwin_core_numbering.R
+++ specify/darwin_core_numbering.R
@@ -1,14 +1,18 @@
 rm(list=ls())
 
-setwd("/Users/marinacostarillo/Documents/Classify-Specify/specify")
-
 library(xlsx)
 library(dplyr)
 library(tidyr)
 
-test <- read.xlsx("seq_numbering_test.xlsx",sheetIndex=1, header = T)
+setwd("/Users/marinacostarillo/Documents/Classify-Specify/specify")
+# list.files()
+
+test <- read.xlsx("Buckley_DC_tabR_image_names.xlsx",sheetIndex=1, header = T)
 image_name_vector <- as.character(test$Image.name)
 
-image_name_df <- as.data.frame(image_name_vector) %>% separate(image_name_vector, into = paste("V", 1:8, sep = "_"))
+image_name_df <- as.data.frame(image_name_vector) %>% separate(image_name_vector, into = paste("V", 1:12, sep = "_"))
 
-cbind(image_name_vector, image_name_df)
+image_name_df <- cbind(image_name = image_name_vector, 
+                  new_sub_indiv = ave(image_name_df$V_4, as.factor(image_name_df$V_1), FUN = seq_along),
+                  image_name_df)
+image_name_df
