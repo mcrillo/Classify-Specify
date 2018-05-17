4efb4196a2c2904fa1d61ff20b7a771f885240ba
Marina Costa Rillo <marina.rillo@evobio.eu>
2018-05-17 14:25:52 +0200

    specimens numbers

--- specify/darwin_core_numbering.R
+++ specify/darwin_core_numbering.R
@@ -7,12 +7,20 @@ library(tidyr)
 setwd("/Users/marinacostarillo/Documents/Classify-Specify/specify")
 # list.files()
 
-test <- read.xlsx("Buckley_Specimens_for_R_tab.xlsx",sheetIndex=1, header = T)
-image_name_vector <- as.character(test$Image.name)
+file <- read.xlsx("Buckley_Specimens_for_R_image_names.xlsx",sheetIndex=1, header = T)
 
-image_name_df <- as.data.frame(image_name_vector) %>% separate(image_name_vector, into = paste("V", 1:12, sep = "_"))
+image_name_vector <- as.character(file$Image.name)
+
+# substituing the double name, otherwise it creates an extra column in function 'separate'
+length(grep(pattern = "aequilateralis-siphonifera", image_name_vector))
+image_name_vector <- sub(pattern="aequilateralis-siphonifera", replacement="siphonifera", x=image_name_vector)
+length(grep(pattern = "aequilateralis-siphonifera", image_name_vector))
+
+image_name_df <- as.data.frame(image_name_vector) %>% separate(image_name_vector, into = paste("V", 1:6, sep = "_"))
 
 image_name_df <- cbind(image_name = image_name_vector, 
                   new_sub_indiv = ave(image_name_df$V_4, as.factor(image_name_df$V_1), FUN = seq_along),
                   image_name_df)
-image_name_df
+head(image_name_df)
+write.csv(image_name_df,"Buckley_Specimens_for_R_numbers.csv", row.names = F)
+
